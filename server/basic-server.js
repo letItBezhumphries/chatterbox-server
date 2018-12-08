3c5d6dbb0e588e6e8581c718c82d793c3c6623e3
EricZH <letItBezhumphries>
2018-12-07 18:09:30 -0800

    Complete urls for chat client

--- server/basic-server.js
+++ server/basic-server.js
@@ -2,11 +2,6 @@
 var http = require('http');
 var handleRequest = require('./request-handler.js'); //
 
-// Every server needs to listen on a port with a unique number. The
-// standard port for HTTP servers is port 80, but that port is
-// normally already claimed by another server and/or not accessible
-// so we'll use a standard testing port like 3000, other common development
-// ports are 8080 and 1337.
 var port = 3000;
 
 // For now, since you're running this server on your local machine,

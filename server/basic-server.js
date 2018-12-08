6b6a8a4da5418b44433a4c289e8020b11a4bac5d
EricZH <letItBezhumphries>
2018-12-07 18:10:28 -0800

    Complete connecting client to server

--- server/basic-server.js
+++ server/basic-server.js
@@ -4,9 +4,6 @@ var handleRequest = require('./request-handler.js'); //
 
 var port = 3000;
 
-// For now, since you're running this server on your local machine,
-// we'll have it listen on the IP address 127.0.0.1, which is a
-// special address that always refers to localhost.
 var ip = '127.0.0.1';
 
 

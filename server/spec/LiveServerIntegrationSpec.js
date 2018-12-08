5347c625be1cb94ef722a4ae11c8b00e7b949ee8
EricZH <letItBezhumphries>
2018-12-07 20:21:54 -0800

    Complete Bare Minimum Requirements

--- server/spec/LiveServerIntegrationSpec.js
+++ server/spec/LiveServerIntegrationSpec.js
@@ -9,6 +9,13 @@ describe('server', function() {
     });
   });
 
+  it('should respond to OPTIONS requests for /classes/messages with a 200 status code', function(done) {
+    request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
   it('should send back parsable stringified JSON', function(done) {
     request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
       expect(JSON.parse.bind(this, body)).to.not.throw();
@@ -64,6 +71,54 @@ describe('server', function() {
         console.log('TEST', messages);
         expect(messages[0].username).to.equal('Jono');
         expect(messages[0].text).to.equal('Do my bidding!');
+        // done();
+      });
+    });
+    request(requestParams, function(error, response, body) {
+      // Now if we request the log, that message we posted should be there:
+      console.log('POST BODY MESSAGES', body);
+
+      request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
+        var messages = JSON.parse(body).results;
+        console.log('TEST', messages);
+        expect(messages[1].username).to.equal('Jono');
+        expect(messages[1].text).to.equal('Do my bidding!');
+        expect(messages.length).to.equal(3);
+        done();
+      });
+    });
+  });
+
+  it('should respond with messages that keep track of correct objectId', function(done) {
+    var requestParams = {method: 'POST',
+      uri: 'http://127.0.0.1:3000/classes/messages',
+      json: {
+        username: 'Jono',
+        text: 'Do my bidding!'}
+    };
+
+    request(requestParams, function(error, response, body) {
+      // Now if we request the log, that message we posted should be there:
+      console.log('POST BODY MESSAGES', body);
+
+      request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
+        var messages = JSON.parse(body).results;
+        console.log('TEST', messages);
+        expect(messages[0].username).to.equal('Jono');
+        expect(messages[0].text).to.equal('Do my bidding!');
+        
+      });
+    });
+    request(requestParams, function(error, response, body) {
+      // Now if we request the log, that message we posted should be there:
+      console.log('POST BODY MESSAGES', body);
+
+      request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
+        var messages = JSON.parse(body).results;
+        console.log('TEST', messages);
+        expect(messages[1].username).to.equal('Jono');
+        expect(messages[1].text).to.equal('Do my bidding!');
+        expect(messages[1].objectId).to.equal(2);
         done();
       });
     });
